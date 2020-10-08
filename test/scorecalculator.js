6960bb9b5d717caa1b0a765a02bcf8a6de09a14b
Lee Gordon <lee.gordon83@gmail.com>
2020-10-08 12:30:48 +0100

    all ones

--- test/scorecalculator.js
+++ test/scorecalculator.js
@@ -1,19 +1,57 @@
 const { expect } = require('chai')
 
-function getScore() {
-  return 0
+
+function getScore(pins) {
+  return pins.reduce((count, frame) => count + (frame[0] + frame[1]), 0)
 }
 
 
 
 describe('scorecalculator', () => {
-  it('all gutter balls returns 0', async () => {
+  it('all gutter balls returns 1', async () => {
     // Arrange
+    const pins = [
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0],
+      [0,0]
+    ]
+
       
     // Act
-    const score = getScore()
+    const score = getScore(pins)
     
     // Assert
     expect(score).to.eql(0)
+  }),
+
+  it('1 pin per roll should return score of 20', async () => {
+    // Arrange
+    const pins = [
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1],
+      [1,1]
+    ]
+
+      
+    // Act
+    const score = getScore(pins)
+    
+    // Assert
+    expect(score).to.eql(20)
   })
+
 })
